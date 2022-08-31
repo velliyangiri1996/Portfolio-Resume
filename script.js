bb08a79fa333bfc403668e79c1dc11bbbdaed993
VELLIYANGIRI RAJAGAESH <57614171+velliyangiri1996@users.noreply.github.com>
2022-08-31 23:35:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+function getHome(){
+    document.getElementById("home").innerHTML = "Hello Home";
+    document.getElementById("home").style.color = "red";
+   let x = document.getElementById("home").style.display = "block"
+   if(x == "block"){
+    
+   }
+    console.log(x)
+}
+
+function getAbout(){
+    document.getElementById("about").innerHTML = "Hello about";
+    document.getElementById("about").style.color = "red";
+}
+
+
+function getSkill(){
+    document.getElementById("skill").innerHTML = "Hello skill";
+    document.getElementById("skill").style.color = "red";
+}
+
+function getContact(){
+    document.getElementById("contact").innerHTML = "Hello con";
+    document.getElementById("contact").style.color = "red";
+}
+
+function getDownload(){
+    document.getElementById("down").innerHTML = "Hello down";
+    document.getElementById("down").style.color = "red";
+}
